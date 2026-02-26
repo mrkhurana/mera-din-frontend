c98ea35da05d18731c1feb0bf09c14b3b8fdfa65
KAMAL KHURANA <kamalkhurana@KAMALs-MacBook-Air.local>
2026-02-27 04:34:43 +0530

    SEO issues fix

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '',
     },
-    sitemap: 'https://mera-din.vercel.app/sitemap.xml',
+    sitemap: 'https://meradinkaisajayega.online/sitemap.xml',
   }
 }
